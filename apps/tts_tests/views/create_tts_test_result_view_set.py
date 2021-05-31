a1aa3e017b04371c3485e9141ca08162e42b22f8
Michał Domino <michal.domino@yahoo.com>
2021-05-31 16:26:19 +0200

    Add CreateListModelMixin to CreateTtsTestResultViewSet and CreateSttTestResultViewSet

--- apps/tts_tests/views/create_tts_test_result_view_set.py
+++ apps/tts_tests/views/create_tts_test_result_view_set.py
@@ -3,9 +3,11 @@ from rest_framework import viewsets, mixins
 from apps.tts_tests.models import TtsTestResult
 from apps.tts_tests.serializers import TtsTestResultSerializer
 from apps.users.permissions import IsVerified
+from shared.mixins import CreateListModelMixin
 
 
 class CreateTtsTestResultViewSet(mixins.CreateModelMixin,
+                                 CreateListModelMixin,
                                  viewsets.GenericViewSet):
     queryset = TtsTestResult.objects.all()
     serializer_class = TtsTestResultSerializer
