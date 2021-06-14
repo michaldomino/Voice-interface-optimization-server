6cf1cf398d74dfe0cee3cf6c140757bd97bd01e5
Michał Domino <michal.domino@yahoo.com>
2021-06-14 23:01:40 +0200

    Add language field to SttTest from Text

--- apps/stt_tests/models/stt_test.py
+++ apps/stt_tests/models/stt_test.py
@@ -1,10 +1,11 @@
 from django.db import models
 
-from apps.texts.models import Text
+from apps.texts.models import Text, TextLanguage
 
 
 class SttTest(models.Model):
     text = models.OneToOneField(Text, on_delete=models.RESTRICT)
+    language = models.ForeignKey(TextLanguage, on_delete=models.RESTRICT)
     text2 = models.TextField()
 
     def __str__(self):
