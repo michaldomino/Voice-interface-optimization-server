8c55f0d6a1b1f6bba9afa8c6f07432f4576a2acc
Michał Domino <michal.domino@yahoo.com>
2021-06-16 20:47:52 +0200

    Applied changes to SttTestSerializer and TttTestSerializer

--- apps/stt_tests/serializers/stt_test_serializer.py
+++ apps/stt_tests/serializers/stt_test_serializer.py
@@ -4,14 +4,6 @@ from apps.stt_tests.models import SttTest
 
 
 class SttTestSerializer(serializers.ModelSerializer):
-    text = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='text'
-    )
-    language = serializers.CharField(
-        source='text.language.code'
-    )
-
     class Meta:
         model = SttTest
         fields = '__all__'
--- apps/tts_tests/serializers/tts_test_serializer.py
+++ apps/tts_tests/serializers/tts_test_serializer.py
@@ -4,14 +4,6 @@ from apps.tts_tests.models import TtsTest
 
 
 class TtsTestSerializer(serializers.ModelSerializer):
-    text = serializers.SlugRelatedField(
-        read_only=True,
-        slug_field='text'
-    )
-    language = serializers.CharField(
-        source='text.language.code'
-    )
-
     class Meta:
         model = TtsTest
         fields = '__all__'
