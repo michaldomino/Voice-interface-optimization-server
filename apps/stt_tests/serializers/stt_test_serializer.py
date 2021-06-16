c32df3d00f64c5dff345733832430d01eebde376
Michał Domino <michal.domino@yahoo.com>
2021-06-16 20:54:01 +0200

    Fixed getting code from language

--- apps/stt_tests/serializers/stt_test_serializer.py
+++ apps/stt_tests/serializers/stt_test_serializer.py
@@ -4,6 +4,10 @@ from apps.stt_tests.models import SttTest
 
 
 class SttTestSerializer(serializers.ModelSerializer):
+    language = serializers.CharField(
+        source='language.code'
+    )
+
     class Meta:
         model = SttTest
         fields = '__all__'
--- apps/tts_tests/serializers/tts_test_serializer.py
+++ apps/tts_tests/serializers/tts_test_serializer.py
@@ -4,6 +4,10 @@ from apps.tts_tests.models import TtsTest
 
 
 class TtsTestSerializer(serializers.ModelSerializer):
+    language = serializers.CharField(
+        source='language.code'
+    )
+
     class Meta:
         model = TtsTest
         fields = '__all__'
