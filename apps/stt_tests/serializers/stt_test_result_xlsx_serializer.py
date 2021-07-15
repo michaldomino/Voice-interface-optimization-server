a7eeb856972757147ab3f3eb4004c37061d83333
Michał Domino <michal.domino@yahoo.com>
2021-07-15 22:25:23 +0200

    Add words count and edit distance fields to stt tests xlsx

--- apps/stt_tests/serializers/stt_test_result_xlsx_serializer.py
+++ apps/stt_tests/serializers/stt_test_result_xlsx_serializer.py
@@ -6,14 +6,44 @@ from apps.stt_tests.models import SttTestResult
 class SttTestResultXlsxSerializer(serializers.ModelSerializer):
     language = serializers.CharField(source='stt_test.language.code')
     text = serializers.SerializerMethodField()
+    words_count = serializers.SerializerMethodField()
     formatted_result = serializers.SerializerMethodField()
+    edit_distance = serializers.SerializerMethodField()
 
     class Meta:
         model = SttTestResult
-        fields = ['language', 'text', 'result', 'formatted_result']
+        fields = ['language', 'text', 'words_count', 'result', 'formatted_result', 'edit_distance']
 
     def get_text(self, obj):
         return obj.stt_test.text.lower()
 
+    def get_words_count(self, obj):
+        return len(obj.stt_test.text.split())
+
+    def get_edit_distance(self, obj):
+        text = obj.stt_test.text.lower()
+        result = obj.result.lower()
+        return self._levenshtein(text, result)
+
     def get_formatted_result(self, obj):
         return obj.result.lower()
+
+    @classmethod
+    def _levenshtein(cls, s1, s2):
+        """Source: https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance"""
+        if len(s1) < len(s2):
+            return cls._levenshtein(s2, s1)
+        if len(s2) == 0:
+            return len(s1)
+
+        previous_row = range(len(s2) + 1)
+        for i, c1 in enumerate(s1):
+            current_row = [i + 1]
+            for j, c2 in enumerate(s2):
+                insertions = previous_row[j + 1] + 1
+                deletions = current_row[j] + 1
+                substitutions = previous_row[j] + (c1 != c2)
+                current_row.append(min(insertions, deletions, substitutions))
+            previous_row = current_row
+
+        return previous_row[-1]
